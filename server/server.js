bf9919a9d3d53a33c837310b4e51c57473d47a7f
B <bgamut@gmail.com>
2021-01-12 20:49:33 +0900

    tried tchunoo's suggestion

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname,'..','public');
-app.use(express.static(publicPath))
-
+//const publicPath = path.join(__dirname,'..','public');
+//app.use(express.static(publicPath))
+app.use(express.static('public'))
 app.get('*', (req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'))
 })
