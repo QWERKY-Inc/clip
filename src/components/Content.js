103be65f29c9d97e06723fa2d8fab01acb3f8dbe
B <bgamut@gmail.com>
2021-01-12 10:42:11 +0900

    add react-native and new design layout via zeplin

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import IntroImg from '../assets/intro.png';
+import icon1 from '../assets/icon1.svg'
+import icon2 from '../assets/icon2.svg'
+import icon3 from '../assets/icon3.svg'
 import './content.css';
 
-const Content=() => {
 
+const Content=() => {
+	const filler=(
+		<div className="headerFiller">
+		</div>
+	)
+	const introImage=(
+		<div className="introImage">
+			<div id="introImageDiv">
+				<img id="introImage" src={IntroImg} alt="Logo" title="Logo" >
+				</img>
+				<div id="introduction">
+					<p className="titleFont introText">온라인 자재 라이브러리</p>
+					<p className="regularFont introText">쉽고 빠르게 자재를 찾아보고</p>
+					<p className="regularFont introText">샘플을 받아볼 수 있는 방법!</p>
+					<p className="regularFont introText">디자인 전문가라면 언제나 무료!</p>
+				</div>
+			</div>
+		</div>
+	)
+	const howItWorks=(
+		<div className="howItWorksDiv">
+			<p className="titleFont">다양한 자재를 검색하고, 다음날 오전까지 샘플을 받아보세요!</p>
+			<div>
+				<button id="howItWorksLink">사용방법</button>
+			</div>
+			<div>
+				<div className='howToIconsDiv'>
+					<img className='howToIcons'src={icon1}></img>
+				</div>
+			</div>
+			<p className="subTitleFont">쉽고 간편하게 검색</p>
+			<p className="regularFont">국내 및 해외 건축 인테리어 브랜드 및 자재를 한곳에서 쉽고 빠르게 검색해보세요.</p>
+			<img className='howToIcons'src={icon2}></img>
+			<p className="subTitleFont">샘플을 주문하면</p>
+			<p className="regularFont">프로젝트에 필요한 샘플을 장바구니에 담고 주문하세요.</p>
+			<img className='howToIcons'src={icon3}></img>
+			<p className="subTitleFont">한박스로 당일발송</p>
+			<p className="regularFont">주문한 샘플은 당일발송 됩니다. (영업일 기준 1-2일 배송완료)</p>
+
+		</div>
+	)
   const data=(
   	<div className="box">
   		<h2>My div content </h2>
@@ -11,13 +55,9 @@ const Content=() => {
 
   return (
     <main className="content">
-	    {data}
-	    {data}
-	    {data}
-	    {data}
-	    {data}
-	    {data}
-	    {data}
+		{filler}
+		{introImage}
+		{howItWorks}
 	    {data}
 	    {data}
     </main>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React,{useEffect} from 'react';
 import './navbar.css';
-import Logo from './../logo.svg';
+// import Logo from './../logo.svg';
+import {TouchableOpacity,Text,View} from 'react-native';
+import Logo from '../assets/header_logo.png'
+import { nativeTouchData } from 'react-dom/test-utils';
 
 const Navbar=() => {
   const [scrolled,setScrolled]=React.useState(false);
@@ -25,36 +28,120 @@ const Navbar=() => {
   return (
     <header className={x.join(" ")}>
       <div className="headerContainer">
-        <button type="button" className="navToggle">
-          <i id="menuBar"class="fas fa-bars"></i>
-        </button>
         <div className="logo">
           <img src={Logo} alt="Logo" title="Logo" />
         </div>
-        <div className="headerLinks">
-          <div className="headerLinksAccount">
-            <button className='linkButton'>             
-              로그인
-            </button>
-          </div>
-          <div className="headerLinksJoin">
-            <button className='linkButton'>
+            <TouchableOpacity
+                  style={{
+                      position:'fixed',
+                      height:50,
+                      width:200,
+                      top:35,
+                      left:'187pt',
+                      backgroundColor:'transparent',
+                      zIndex:101
+                  }}
+                  activeOpacity={0.5}
+              >
+                <Text
+                selectable={false} 
+                style ={{
+                    fontSize: '25pt',
+                    fontWeight:'700',
+                    textDecorationLine:'none',
+                    color:'black',
+                    // textShadowColor: 'rgba(0, 0, 0, 0.85)',
+                    // textShadowOffset: {width: 0, height: 0},
+                    // textShadowRadius: 2,
+                    textAlign:'center',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    flexDirection:'row',
+                    margin:11,
+                    padding:'auto',
+                    pointerEvents:'none'
+                }}
+                >
+                  자체카테고리
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                  style={{
+                      position:'fixed',
+                      height:50,
+                      width:150,
+                      top:35,
+                      left:'349pt',
+                      backgroundColor:'transparent',
+                      zIndex:101
+                  }}
+                  activeOpacity={0.5}
+              >
+                <Text
+                selectable={false} 
+                style ={{
+                    fontSize: '25pt',
+                    fontWeight:'700',
+                    textDecorationLine:'none',
+                    // color:'white',
+                    // textShadowColor: 'rgba(0, 0, 0, 0.85)',
+                    // textShadowOffset: {width: 0, height: 0},
+                    // textShadowRadius: 2,
+                    color:'black',
+                    textAlign:'center',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    flexDirection:'row',
+                    margin:11,
+                    padding:'auto',
+                    pointerEvents:'none'
+                }}
+                >
+                  사공부위
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                  style={{
+                      position:'fixed',
+                      height:50,
+                      width:120,
+                      top:35,
+                      left:'478pt',
+                      backgroundColor:'transparent',
+                      zIndex:101
+                  }}
+                  activeOpacity={0.5}
+              >
+                <Text
+                selectable={false} 
+                style ={{
+                    fontSize: '25pt',
+                    fontWeight:'700',
+                    textDecorationLine:'none',
+                    // color:'white',
+                    // textShadowColor: 'rgba(0, 0, 0, 0.85)',
+                    // textShadowOffset: {width: 0, height: 0},
+                    // textShadowRadius: 2,
+                    color:'black',
+                    textAlign:'center',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    flexDirection:'row',
+                    margin:11,
+                    padding:'auto',
+                    pointerEvents:'none'
+                }}
+                >
+                  브랜드
+                </Text>
+            </TouchableOpacity>
+          {/* </div> */}
+          {/* <div className="headerLinksJoin"> */}
+            {/* <button className='linkButton'>
               가입
-            </button>
-          </div>
-        </div>
-        <div className="headerMobileSearch">
-          <div className="miniSearch">
-            <form className="inputPlaceholder">
-              <div className="inputPlaceholder">
-                <input id='searchBar'type='text' ></input>
-              </div>
-              <button id='submitButton'type='submit'>
-                <i class="fas fa-search"></i>
-              </button>
-            </form>
-          </div>
-        </div>
+            </button> */}
+             
+
       </div>
        
 
