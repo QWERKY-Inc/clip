9fc572ee9449c7a6e8d07e6609673c019e17bfda
B <bgamut@gmail.com>
2021-01-13 13:07:27 +0900

    cors added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const path = require('path')
-
 const app = express();
-const port = process.env.PORT || 3000;
+const cors = require('cors')
+const port = process.env.PORT || 8080;
 //const publicPath = path.join(__dirname,'..','public');
 //app.use(express.static(publicPath))
-app.use(express.static('public'))
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(publicPath,'index.html'))
+const buildPath = path.join(__dirname,'..','build')
+console.log(buildPath)
+app.use(express.static(buildPath))
+// app.get('*', (req,res)=>{
+//     // res.sendFile(path.join(publicPath,'index.html'))
+//     res.sendFile(path.join(__dirname,'..','build','index.html'))
+// })
+app.use(cors());
+app.get('/', function (req, res) {
+    res.json(path.join(__dirname, '..','build', 'index.html'))
+    // res.sendFile(path.join(__dirname, '..','build', 'index.html'));
+  });
+app.get('/data',(req,res)=>{
+    const data = {
+        lastname : "han",
+        firstname : "driver"
+    };
+    res.json(data);
 })
+
 app.listen(port,()=>{
     console.log('server is up on port ' + port);
 })
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Navbar from './Navbar';
 import Content from './Content';
 import NavBarFiller from './NavBarFiller';
@@ -5,14 +6,23 @@ import {TouchableOpacity,Text,View,Modal,TouchableHighlight} from 'react-native'
 
 function Category() {
 
+    const[data,setData]=React.useState({})
+    fetch('/data')
+    .then(res=>res.json())
+    .then(data=>setData(data),()=>{
+      console.log('data read : ' , data);
+    })
     return (
       <div className="Category">
        
           <Navbar />
           <NavBarFiller/>
           <Text> 
-            Category
+            Category Data
           </Text> 
+          <Text>
+            {data.lastname} {data.firstname}
+          </Text>
           <Content/>
             
           
