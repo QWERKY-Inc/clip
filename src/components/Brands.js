961cc34d02347403925dec02f78a8ea6f0aa31c5
B <bgamut@gmail.com>
2021-01-22 11:37:35 +0900

    ct_id parses in brands

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,9 +1,27 @@
+import React,{useEffect} from 'react';
 import Navbar from './Navbar';
 import Content from './Content';
 import NavBarFiller from './NavBarFiller';
 import {TouchableOpacity,Text,View,Modal,TouchableHighlight} from 'react-native';
+const queryString = require('query-string');
 
-function Brands() {
+function Brands(props) {
+
+  const[data,setData]=React.useState({})
+  const[catQ,setCatQ]=React.useState({cat_num: null})
+
+  useEffect(() => {
+      
+    // fetch('/data')
+    // .then(res=>res.json())
+    // .then(data=>setData(data),()=>{
+    //   console.log('data read : ' , data);
+    // })
+    const parsed = queryString.parse(props.location.search);
+
+    console.log(parsed)
+    setCatQ(parsed)
+  },[])
     return (
       <div className="Brands">
         <Navbar />
@@ -11,6 +29,10 @@ function Brands() {
         <Text> 
           Brands
         </Text> 
+        <br></br>
+          <Text>
+            {catQ.ct_id}
+          </Text>
         <Content/>  
       </div>
     );
