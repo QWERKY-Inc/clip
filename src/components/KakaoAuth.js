ecb5a438b7fae38ebde7e6abbd49696eb976bee6
tkahng <tkahng@gmail.com>
2021-03-08 03:44:21 +0900

    added kakao login componet

---
+++ src/components/KakaoAuth.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import KaKaoLogin from "react-kakao-login";
+
+const KakaoAuth = () => {
+  const [profile, setProfile] = useState(null);
+
+  responseKaKao = (res) => {};
+
+  return <div></div>;
+};
+
+export default KakaoAuth;
