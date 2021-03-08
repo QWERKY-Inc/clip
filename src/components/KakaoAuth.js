56a07eee5fffadca1a2d612e80a2982cbcf08870
tkahng <tkahng@gmail.com>
2021-03-08 15:48:16 +0900

    Added Kakao auth. can create required body for api

--- src/components/KakaoAuth.js
+++ src/components/KakaoAuth.js
@@ -1,12 +1,37 @@
+import KakaoLogin from "react-kakao-login";
 import { useState } from "react";
-import KaKaoLogin from "react-kakao-login";
+import { token } from "./Secrets";
+// Secrets.js is gitignored until i find a way to handle secrets
 
 const KakaoAuth = () => {
   const [profile, setProfile] = useState(null);
 
-  responseKaKao = (res) => {};
+  // Does this need to be async? or even useState?
+  const responseKaKao = (res) => {
+    // setProfile({data: res});
+    // const data = res;
 
-  return <div></div>;
+    const loginBody = {
+      mem_jointype: "KAKAO",
+      mem_email: res.profile.kakao_account.email,
+      mem_snsid: res.profile.id,
+      mem_token: null,
+    };
+
+    console.log(loginBody);
+  };
+
+  return (
+    <div>
+      <KakaoLogin
+        token={token}
+        onSuccess={responseKaKao}
+        onFail={console.error}
+        onLogout={console.info}
+        getProfile={true}
+      />
+    </div>
+  );
 };
 
 export default KakaoAuth;
